d8be0c4ef944a4badce0fe84b618650221f76b8e
Kevin-gram <k.nyiringan@alustudent.com>
2024-06-07 23:44:02 +0200

    columns and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
-import './pages/HomePage.dart';
-import './pages/OnGoScreen.dart';
+import 'package:untitled1/product_list_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,17 +8,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Navigation Assignemt',
-      home: HomeScreen(),
+      title: 'Flutter Demo',
       theme: ThemeData(
-          appBarTheme: AppBarTheme(backgroundColor: Colors.blueGrey[900]),
-          iconTheme: IconThemeData(color: Colors.white)),
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ProductListPage(),
     );
   }
 }
-
-
